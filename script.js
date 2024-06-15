8845d49e71e5026ce3b8676dac17ac31af69b5f8
woojin <114149553+danawls@users.noreply.github.com>
2024-06-15 23:34:59 +0900

    upload

---
+++ script.js
@@ -0,0 +1,115 @@
+const userInput = document.querySelector("input");
+const RNA = document.querySelector("#rna");
+const codon = document.querySelector("#codon");
+const aminoAcid = document.querySelector("#amino-acid");
+const protein = document.querySelector("#protein");
+const form = document.querySelector("form");
+const footer = document.querySelector("footer");
+
+const CODONRNA = {UUU: "Phe", UUC: "Phe", UUA: "Leu", UUG: "Leu", CUU: "Leu", CUC: "Leu", CUA: "Leu", CUG: "Leu", AUU: "Ile", AUC: "Ile", AUA: "Ile", AUG: "Met", GUU: "Val", GUC: "Val", GUA: "Val", GUG: "Val", UCU: "Ser", UCC: "Ser", UCA: "Ser", UCG: "Ser", CCU: "Pro", CCC: "Pro", CCA: "Pro", CCG: "Pro", ACU: "Thr", ACC: "Thr", ACA: "Thr", ACG: "Thr", GCU: "Ala", GCC: "Ala", GCA: "Ala", GCG: "Ala", UAU: "Tyr", UAC: "Tyr", UAA: "Stop", UAG: "Stop", CAU: "His", CAC: "His", CAA: "Gln", CAG: "Gln", AAU: "Asn", AAC: "Asn", AAA: "Lys", AAG: "Lys", GAU: "Asp", GAC: "Asp", GAA: "Glu", GAG: "Glu", UGU: "Cys", UGC: "Cys", UGA: "Stop", UGG: "Trp", CGU: "Arg", CGC: "Arg", CGA: "Arg", CGG: "Arg", AGU: "Ser", AGC: "Ser", AGA: "Arg", AGG: "Arg", GGU: "Gly", GGC: "Gly", GGA: "Gly", GGG: "Gly"};
+
+let DNAVal, RNAVal = [], aminos, proteins = [], codons;
+
+const takeDNA = function(event) {
+    event.preventDefault();
+    DNAVal = userInput.value;
+    transcript();
+    if(transcript() === 1) {
+        return;
+    }
+    translation();
+}
+
+const transcript = function() {
+    let index = 0;
+    for(const char of DNAVal) {
+        if(char == "A") {
+            RNAVal[index] = "U";
+        } else if(char == "T") {
+            RNAVal[index] = "A";
+        } else if(char == "G") {
+            RNAVal[index] = "C";
+        } else if(char == "C") {
+            RNAVal[index] = "G";
+        } else {
+            RNA.textContent = "RNA: INVALID VALUE";
+            return 1;
+        }
+        index++;
+        RNA.textContent = `RNA: ${RNAVal.join("")}`;
+    }
+}
+
+const getCodon = function(c) {
+    codons = c;
+    codon.textContent = `코돈: ${codons}`;
+    for(const val in CODONRNA) {
+        if(codons == val) {
+            aminos = CODONRNA[val];
+            aminoAcid.textContent = `아미노산: ${CODONRNA[val]}`;
+        }
+    }
+}
+
+const translation = function() {
+    let isStart = false, isEnd = false, sIndex, eIndex;
+    for(let k = 2; k < RNAVal.length; k++) {
+        getCodon(RNAVal[k - 2] + RNAVal[k - 1] + RNAVal[k]);
+        if(aminos == "Met"&&!isStart) {
+            isStart = true;
+            sIndex = k;
+            for(let h = k; h < RNAVal.length; h += 3) {
+                getCodon(RNAVal[h - 2] + RNAVal[h - 1] + RNAVal[h]);
+                if(aminos == "Stop" && !isEnd) {
+                    isEnd = true;
+                    eIndex = h;
+                }
+            }
+        }
+    }
+
+    if(!isStart) {
+        protein.textContent = "단백질: THERE IS NO START";
+        return;
+    } else if(!isEnd) {
+        protein.textContent = "단백질: THERE IS NO END";
+        return;
+    }
+
+    for(let j = sIndex; j < eIndex; j += 3) {
+        getCodon(RNAVal[j - 2] + RNAVal[j - 1] + RNAVal[j])
+        proteins.push(aminos);
+        protein.textContent = `단백질: ${proteins.toString()}`;
+    }
+}
+
+form.addEventListener("submit", takeDNA);
+footer.addEventListener("click", () => {
+    window.location.reload();
+});
+
+const checkCopy = function(toC, t) {
+    if(!toC) {
+        alert(`${t} value is invalid. You can't copy it.`);
+    }
+}
+
+RNA.addEventListener("click", () => {
+    checkCopy(RNAVal.join(""), "RNA");
+    navigator.clipboard.writeText(RNAVal.join("")).catch((r) => {alert(r);});
+})
+
+codon.addEventListener("click", () => {
+    checkCopy(codons, "CODON");
+    navigator.clipboard.writeText(codons).catch((r) => {alert(r);});
+})
+
+aminoAcid.addEventListener("click", () => {
+    checkCopy(aminos, "AMINO ACID");
+    navigator.clipboard.writeText(aminos).catch((r) => {alert(r);});
+})
+
+protein.addEventListener("click", () => {
+    checkCopy(proteins.toString(), "PROTEIN");
+    navigator.clipboard.writeText(proteins.toString()).catch((r) => {alert(r);});
+})
---
+++ test_rna.test.js
@@ -0,0 +1,78 @@
+const puppeteer = require("puppeteer");
+
+let DNA;
+
+describe("first tase case", () => {
+    let browser, page;
+    beforeEach(async () => {
+        DNA = "ACTACGATCGACGTAAGCTAATT";
+        browser = await puppeteer.launch({headless: false, slowMo: 80});
+        page = await browser.newPage();
+        await page.goto("file:///Users/danawls/Desktop/DNA-RNA-Protein/index.html?");
+    });
+
+    afterEach(() => {
+        DNA = null;
+        browser.close();
+    });
+
+    test("Get Test 1st", async () => {
+        const isel = "#dna";
+        await page.click("#dna");
+        await page.type("#dna", DNA);
+        await page.keyboard.press("Enter");
+        let pro = await page.$eval("#protein", el => el.textContent);
+        expect(pro).toBe("단백질: Met,Leu,Ala,Ala,Phe,Asp");
+        DNA = "CTACTACGATCGACGTAAGC";
+        // const browser = await puppeteer.launch({headless: false, slowMo: 80});
+        // const page = await browser.newPage();
+        // await page.goto("file:///Users/danawls/Desktop/DNA-RNA-Protein/index.html?");
+        await page.evaluate(() => {document.querySelector("#dna").value = "";});
+        await page.type("#dna", DNA);
+        await page.keyboard.press("Enter");
+        pro = await page.$eval("#protein", el => el.textContent);
+        expect(pro).toBe("단백질: THERE IS NO END");
+        DNA = "TCTAGTGATCGACGTAAGCAA";
+        // const browser = await puppeteer.launch({headless: false, slowMo: 80});
+        // const page = await browser.newPage();
+        // await page.goto("file:///Users/danawls/Desktop/DNA-RNA-Protein/index.html?");
+        await page.evaluate(() => {document.querySelector("#dna").value = "";});
+        await page.type("#dna", DNA);
+        await page.keyboard.press("Enter");
+        pro = await page.$eval("#protein", el => el.textContent);
+        expect(pro).toBe("단백질: THERE IS NO START");
+    });
+
+    // test("Get Test 2nd",async () => {
+    //     DNA = "CTACTACGATCGACGTAAGC";
+    //     // const browser = await puppeteer.launch({headless: false, slowMo: 80});
+    //     // const page = await browser.newPage();
+    //     // await page.goto("file:///Users/danawls/Desktop/DNA-RNA-Protein/index.html?");
+    //     for (let i = 0; i < 2; ++i) {
+    //         await page.click("#dna");
+    //     }
+    //     await page.keyboard.press("Delete");
+    //     await page.type("#dna", DNA);
+    //     await page.type("#dna", DNA);
+    //     await page.keyboard.press("Enter");
+    //     const pro = await page.$eval("#protein", el => el.textContent);
+    //     expect(pro).toBe("단백질: THERE IS NO END");
+    // });
+    //
+    // test("Get Test 2nd",async () => {
+    //     DNA = "TCTAGTGATCGACGTAAGCAA";
+    //     // const browser = await puppeteer.launch({headless: false, slowMo: 80});
+    //     // const page = await browser.newPage();
+    //     // await page.goto("file:///Users/danawls/Desktop/DNA-RNA-Protein/index.html?");
+    //     for (let i = 0; i < 2; ++i) {
+    //         await page.click("#dna");
+    //     }
+    //     await page.keyboard.press("Delete");
+    //     await page.type("#dna", DNA);
+    //     await page.type("#dna", DNA);
+    //     await page.keyboard.press("Enter");
+    //     const pro = await page.$eval("#protein", el => el.textContent);
+    //     expect(pro).toBe("단백질: THERE IS NO START");
+    // });
+});
+
